054287f0c3154d6afd653544165ac4f706d42f42
kiku ishimoto <kikuish@gmail.com>
2020-06-04 18:38:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,4 +97,13 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(number)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ? ORDER BY students.id 
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
 end
